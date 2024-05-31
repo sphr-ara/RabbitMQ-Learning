f086fd37c7752c577aa146fc119ba9167384d4c7
Sepehr Arami <cap.s.arami@outlook.com>
2024-05-31 12:59:53 +0330

    [ADD] Competing Consumers concept

---
+++ MyPublisher/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "letterbox", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+var random = new Random();
+
+var messageId = 1;
+
+while (true)
+{
+    var publishingTime = random.Next(1, 4);
+    
+    var message = $"Sending MessageID: {messageId}";
+
+    var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+    // "" in first parameter will set the exchange to default exchange
+    channel.BasicPublish("", "letterbox", null, encodedMessage);
+
+    Console.WriteLine($"Published message: {message}");
+
+    Task.Delay(TimeSpan.FromSeconds(publishingTime)).Wait();
+    
+    messageId++;
+}
