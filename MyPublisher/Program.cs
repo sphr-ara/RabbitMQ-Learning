803e7437b0ec3fba0d571ccd4a919faf3868a8a4
Sepehr Arami <cap.s.arami@outlook.com>
2024-05-31 14:18:07 +0330

    Publisher/Subscriber pattern

--- MyPublisher/Program.cs
+++ MyPublisher/Program.cs
@@ -10,26 +10,14 @@ using var connection = factory.CreateConnection();
 
 using var channel = connection.CreateModel();
 
-channel.QueueDeclare(queue: "letterbox", durable: false, exclusive: false, autoDelete: false, arguments: null);
+// channel.QueueDeclare(queue: "letterbox", durable: false, exclusive: false, autoDelete: false, arguments: null);
 
-var random = new Random();
+channel.ExchangeDeclare(exchange: "pubsub", type: ExchangeType.Fanout);
 
-var messageId = 1;
+var message = "Hello I want to broadcast this message.";
 
-while (true)
-{
-    var publishingTime = random.Next(1, 4);
-    
-    var message = $"Sending MessageID: {messageId}";
+var encodedMessage = Encoding.UTF8.GetBytes(message);
 
-    var encodedMessage = Encoding.UTF8.GetBytes(message);
+channel.BasicPublish("pubsub", "", null, encodedMessage);
 
-    // "" in first parameter will set the exchange to default exchange
-    channel.BasicPublish("", "letterbox", null, encodedMessage);
-
-    Console.WriteLine($"Published message: {message}");
-
-    Task.Delay(TimeSpan.FromSeconds(publishingTime)).Wait();
-    
-    messageId++;
-}
+Console.WriteLine($"Published message: {message}");
